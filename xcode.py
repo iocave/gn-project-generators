55ceee9426a4b63d58e70b6fa2067b6ce8937947
Matej Knopp <matej.knopp@gmail.com>
2016-07-27 16:43:52 +0200

    Add All Targets product target to build everything

--- xcode.py
+++ xcode.py
@@ -21,14 +21,14 @@ from impl.common import *
 class ProjectGenerator:
 
     def __init__(self, project_definition, project_name):
-        
+
         self.project_definition = project_definition
 
         self.container = PBXContainer()
 
         # every build object must be added to self.objects
         self.objects = self.container.get_objects()
-        
+
         self.project = PBXProject(self.objects, project_name)
 
         self.objects.add_object(self.project)
@@ -40,18 +40,18 @@ class ProjectGenerator:
 
         self.objects.add_object(self.project_bc)
         self.project_bcl.add_build_configuration(self.project_bc)
-    
+
         self.project_bcl.set_property("defaultConfigurationIsVisible", 1)
         self.project_bcl.set_property("defaultConfigurationName", self.project_bc.get_name())
-        
-        self.project.set_build_configuration_list(self.project_bcl)        
-                
+
+        self.project.set_build_configuration_list(self.project_bcl)
+
         self.main_group = PBXGroup(self.objects)
         self.objects.add_object(self.main_group)
-            
+
         self.project.set_main_group(self.main_group)
 
-        self.container.set_root_object(self.project) 
+        self.container.set_root_object(self.project)
 
     def get_project_name(self):
         return self.project.get_name()
@@ -65,8 +65,8 @@ class ProjectGenerator:
 
         assert path.startswith("//")
         path = path[2:]
-        
-        segments = path.split("/")                
+
+        segments = path.split("/")
 
         current_parent = self.main_group
         for segment in segments:
@@ -85,9 +85,9 @@ class ProjectGenerator:
             current_parent = group
 
         return current_parent
-        
+
     def _extract_dialect(self, current_value, flags, is_cc):
-        if current_value is None:            
+        if current_value is None:
             for flag in flags:
                 if (flag.startswith("-std=")):
                     current_value = flag[5:]
@@ -96,19 +96,19 @@ class ProjectGenerator:
         # we're looking for c++ dialect, it must have + in name
         if current_value is not None and is_cc and current_value.find("+") == -1:
             current_value = None
-        
+
         # we're looking for c dialect, it must not have + in name
         if current_value is not None and not is_cc and current_value.find("+") != -1:
             current_value = None
 
         return current_value
-    
+
     #
     # Indexing targets
     #
 
     def _generate_indexing_target(self, project_target, compilable_references):
-        name = project_target.name        
+        name = project_target.name
         name = name[2:]
         product_name = name.replace("/", "_")
         product_name = name.replace(":", "_")
@@ -117,12 +117,12 @@ class ProjectGenerator:
         native_target = PBXNativeTarget(self.objects, name, product_name, "com.apple.product-type.library.static")
         self.objects.add_object(native_target)
         self.project.add_target(native_target)
-                 
+
         # create source build phase and add it to target
         sources_bf = PBXSourcesBuildPhase(native_target)
         self.objects.add_object(sources_bf)
         native_target.add_build_phase(sources_bf)
-        
+
         # add build files to source build phase
         for reference in compilable_references:
             build_file = PBXBuildFile(sources_bf, reference, sources_bf)
@@ -130,10 +130,10 @@ class ProjectGenerator:
             self.objects.add_object(build_file)
 
         # add build configuration list to target
-        target_bcl = XCConfigurationList(native_target, native_target)        
+        target_bcl = XCConfigurationList(native_target, native_target)
         self.objects.add_object(target_bcl)
         native_target.set_build_configuration_list(target_bcl)
-        
+
         # add build configuration to build configuration list
         target_bc = XCBuildConfiguration(target_bcl, "Default")
         self.objects.add_object(target_bc)
@@ -156,7 +156,7 @@ class ProjectGenerator:
         # try to extract dialect
         dialect_c = self._extract_dialect(None, project_target.cflags_c, False)
         dialect_c = self._extract_dialect(dialect_c, project_target.cflags_objc, False)
-        dialect_c = self._extract_dialect(dialect_c, project_target.cflags, False)        
+        dialect_c = self._extract_dialect(dialect_c, project_target.cflags, False)
         dialect_cc = self._extract_dialect(None, project_target.cflags_cc, True)
         dialect_cc = self._extract_dialect(dialect_cc, project_target.cflags_objcc, True)
         dialect_cc = self._extract_dialect(dialect_cc, project_target.cflags, True)
@@ -170,7 +170,7 @@ class ProjectGenerator:
         if precompiled_header is not None:
             target_bc.build_settings().update({
                 "GCC_PREFIX_HEADER" : project_target.project.get_relative_path(precompiled_header),
-                "GCC_PRECOMPILE_PREFIX_HEADER" : "YES",                
+                "GCC_PRECOMPILE_PREFIX_HEADER" : "YES",
                 "GCC_INCREASE_PRECOMPILED_HEADER_SHARING" : "YES", # Allow sharing headers between targets
                 "PRECOMPS_INCLUDE_HEADERS_FROM_BUILT_PRODUCTS_DIR" : "NO"
             })
@@ -185,14 +185,14 @@ class ProjectGenerator:
         done_sources = set()
 
         for target_name, target in self.project_definition.targets.items():
-        
+
             # only deal with buildable targets
             if (target.type != TargetType.executable and
                 target.type != TargetType.static_library and
                 target.type != TargetType.shared_library and
                 target.type != TargetType.loadable_module and
                 target.type != TargetType.source_set and
-                target.type != TargetType.build_dir): 
+                target.type != TargetType.build_dir):
                 continue
 
             # ignore non default targets
@@ -202,8 +202,8 @@ class ProjectGenerator:
             compilable_references = []
 
             sources = list(target.sources)
-            if target.get_precompiled_header():                
-                sources.append(target.get_precompiled_header())                
+            if target.get_precompiled_header():
+                sources.append(target.get_precompiled_header())
 
             for source in sources:
 
@@ -215,7 +215,7 @@ class ProjectGenerator:
 
                 folder_path, file_name = posixpath.split(source)
                 group = self.group_for_path(folder_path)
-                
+
                 # add file reference for the source if not there yet
                 file = group.get_child(file_name)
                 if file is None:
@@ -223,14 +223,14 @@ class ProjectGenerator:
                     path = None
 
                     if group == self.main_group:
-                        root = self.project_definition.get_relative_path("//")                        
-                        path = root + "/" + source[2:]                        
+                        root = self.project_definition.get_relative_path("//")
+                        path = root + "/" + source[2:]
 
                     file = PBXFileReference(group, file_name, path)
                     group.add_child(file)
                     self.objects.add_object(file)
 
-                    ext = file.ext                    
+                    ext = file.ext
                     if ext in set(['c', 'cc', 'cxx', 'cpp', 'm', 'mm']):
                         compilable_references.append(file)
 
@@ -243,26 +243,26 @@ class ProjectGenerator:
     #
 
     def _generate_product_target(self, target_name, output_name, output_dir, is_bundle):
-            
-        # Use output name as target name because it is shorter, as long as there isn't 
+
+        # Use output name as target name because it is shorter, as long as there isn't
         # target with such name already
         xcode_target_name = output_name
         if xcode_target_name in self._target_names:
             xcode_target_name = target_name[2:] # remove //
         self._target_names.add(xcode_target_name)
 
-        legacy_target = PBXLegacyTarget(self.objects, xcode_target_name, 
-                                        "/usr/bin/python", 
+        legacy_target = PBXLegacyTarget(self.objects, xcode_target_name,
+                                        "/usr/bin/python",
                                         "invoke_ninja.py " + target_name + " $(ACTION)",
                                          "$(PROJECT_DIR)")
         self.objects.add_object(legacy_target)
         self.project.add_target(legacy_target)
 
         # add build configuration list to target
-        target_bcl = XCConfigurationList(legacy_target, legacy_target)        
+        target_bcl = XCConfigurationList(legacy_target, legacy_target)
         self.objects.add_object(target_bcl)
         legacy_target.set_build_configuration_list(target_bcl)
-        
+
         # add build configuration to build configuration list
         target_bc = XCBuildConfiguration(target_bcl, "Default")
         self.objects.add_object(target_bc)
@@ -271,27 +271,28 @@ class ProjectGenerator:
         target_bcl.set_property("defaultConfigurationIsVisible", 0)
         target_bcl.set_property("defaultConfigurationName", target_bc.get_name())
 
-        relative_output_dir = self.project_definition.get_relative_path(output_dir)
-        if relative_output_dir == "" or relative_output_dir == ".":
-            build_dir = "$(PROJECT_DIR)"
-        else:
-            build_dir = "$(PROJECT_DIR)/" + relative_output_dir
-        
-        target_bc.build_settings().update({
-            "CONFIGURATION_BUILD_DIR" : build_dir,
-            "PRODUCT_NAME" : output_name
-        })
+        if target_name != "alltargets":
+            relative_output_dir = self.project_definition.get_relative_path(output_dir)
+            if relative_output_dir == "" or relative_output_dir == ".":
+                build_dir = "$(PROJECT_DIR)"
+            else:
+                build_dir = "$(PROJECT_DIR)/" + relative_output_dir
 
-        file_ref = PBXFileReference(legacy_target, output_name)
-        if is_bundle:    
-            file_ref.make_build_product_app_bundle()
-        else:
-            file_ref.make_build_product_executable()
+            target_bc.build_settings().update({
+                "CONFIGURATION_BUILD_DIR" : build_dir,
+                "PRODUCT_NAME" : output_name
+            })
 
-        self.objects.add_object(file_ref)
-        self.main_group.add_child(file_ref)
+            file_ref = PBXFileReference(legacy_target, output_name)
+            if is_bundle:
+                file_ref.make_build_product_app_bundle()
+            else:
+                file_ref.make_build_product_executable()
 
-        legacy_target.set_product_reference(file_ref)
+            self.objects.add_object(file_ref)
+            self.main_group.add_child(file_ref)
+
+            legacy_target.set_product_reference(file_ref)
 
     # returns True if target is direct dependency of bundle_data target; we use this to remove
     # executable targets that will be part of bundle
@@ -306,32 +307,34 @@ class ProjectGenerator:
 
         self._target_names = set()
 
+        self._generate_product_target("alltargets", "All Targets", "", False)
+
         for target_name, target in self.project_definition.targets.items():
 
             if target.toolchain != self.project_definition.default_toolchain:
                 continue
 
-            if (target.type == TargetType.executable and 
+            if (target.type == TargetType.executable and
                 not self._target_is_dependency_of_bundle_data(target_name)):
-                                
+
                 output_name = target.get_output_name()
                 output_dir = target.get_output_dir()
 
                 self._generate_product_target(target_name, output_name, output_dir, False)
-            
+
             elif target.type == TargetType.create_bundle:
 
                 if (target.bundle_data.get("product_type", "") == "com.apple.product-type.application"):
                     app_dir, app_name = posixpath.split(target.bundle_data["root_dir_output"])
 
-                    self._generate_product_target(target_name, app_name, app_dir, True)    
+                    self._generate_product_target(target_name, app_name, app_dir, True)
 
     def write_build_script(self):
 
         path = posixpath.join(get_script_dir(), "../tools/invoke_ninja.py")
         with open(path) as f:
             content = f.read()
-            
+
             script_file = self.project_definition.get_absolute_build_path() + "invoke_ninja.py"
             overwrite_file_if_different(script_file, content)
 
@@ -340,19 +343,19 @@ class ProjectGenerator:
     #
 
     def write(self):
-        
+
         output = StringIO()
         self.container.write_object(0, output)
         new_content = output.getvalue()
-        
+
         project_folder = self.project_definition.get_absolute_build_path() + self.project.get_name() + ".xcodeproj"
         if not os.path.exists(project_folder):
             os.makedirs(project_folder)
-    
+
         project_file = project_folder + "/project.pbxproj"
         if not overwrite_file_if_different(project_file, new_content):
             print("No changes detected - will not overwrite project file for " + self.project.get_name())
-        
+
         output.close()
 
 #
@@ -364,7 +367,7 @@ class WorkspaceGenerator:
     def __init__(self, project_definition, workspace_name, project_generators):
         self.workspace_name = workspace_name
         self.project_definition = project_definition
-        self.project_generators = project_generators        
+        self.project_generators = project_generators
 
     def write(self):
 
@@ -380,41 +383,41 @@ class WorkspaceGenerator:
         workspace_folder = self.project_definition.get_absolute_build_path() + self.workspace_name + ".xcworkspace"
         if not os.path.exists(workspace_folder):
             os.makedirs(workspace_folder)
-    
+
         project_file = workspace_folder + "/contents.xcworkspacedata"
         if not overwrite_file_if_different(project_file, new_content):
             print("No changes detected - will not overwrite workspace file")
-        
-        output.close()        
-        
+
+        output.close()
+
 def run():
 
     if len(sys.argv) != 2 and len(sys.argv) != 3:
         print("Usage: " + sys.argv[0] + " <path-to-json-file> [workspace-name]")
         exit(1)
-    
+
     path_to_file = sys.argv[1]
-    workspace_name = "Workspace" 
+    workspace_name = "Workspace"
     if len(sys.argv) == 3:
         workspace_name = sys.argv[2]
-    
+
     with open(path_to_file, "r") as json_file:
         v = json_file.read()
         json_file.close()
         js = json.loads(v)
-        
+
         project = Project(js)
-            
+
         gen_sources = ProjectGenerator(project, "Sources")
-        gen_sources.generate_targets_for_indexing()            
+        gen_sources.generate_targets_for_indexing()
         gen_sources.write()
 
         gen_products = ProjectGenerator(project, "Products")
-        gen_products.generate_targets_for_products()        
+        gen_products.generate_targets_for_products()
         gen_products.write()
         gen_products.write_build_script()
 
-        # Put products first so that when xcode autogenerates schemes product schemes 
+        # Put products first so that when xcode autogenerates schemes product schemes
         # (which are actually relevant) are placed first
         gen_workspace = WorkspaceGenerator(project, workspace_name, [gen_products, gen_sources])
         gen_workspace.write()
