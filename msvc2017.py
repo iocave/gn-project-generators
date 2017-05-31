5abfdcb206a7f71de92e4f9cebb3f24fee3515ad
Matej Knopp <matej.knopp@gmail.com>
2017-05-31 20:14:30 +0200

    add support for msvc2017

---
+++ msvc2017.py
@@ -0,0 +1,30 @@
+#!
+import json
+from impl.msvc import ProjectGenerator
+from impl.common import *
+import sys
+
+def run():
+
+    if len(sys.argv) != 2 and len(sys.argv) != 3:
+        print("Usage: " + sys.argv[0] + " <path-to-json-file> [Solution-name]")
+        exit(1)
+
+    path_to_file = sys.argv[1]
+    solution_name = "Solution"
+    if len(sys.argv) == 3:
+        solution_name = sys.argv[2]
+
+    with open(path_to_file, "r") as json_file:
+        v = json_file.read()
+        json_file.close()
+        js = json.loads(v)
+
+        project = Project(js)
+
+        generator = ProjectGenerator(project, solution_name, tools_version = "15.0", platform_toolset = "v141")
+        count = generator.generate()
+
+        print("Done generating " + str(count) + " project file(s)")
+
+run()
